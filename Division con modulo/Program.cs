855058a1ee56b8abbbb7e25a93f928d0c8290058
Rosa M. Rodriguez <romarola@hotmail.com>
2018-09-07 00:15:39 -0300

    DivisionConModulo

---
+++ Division con modulo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Division_con_modulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
